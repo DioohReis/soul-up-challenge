73b820e757bf56db863457f3ff4577924941b6bd
gabriel <gabrielrazod9j@gmail.com>
2026-09-13 13:44:30 -0300

    feat: Fazendo alteração na descrição do cargo no team.ts

--- src/data/team.ts
+++ src/data/team.ts
@@ -53,7 +53,7 @@ export const team: TeamMember[] = [
     github: 'https://github.com/gabrielrazod9j-ops',
     linkedin: 'https://www.linkedin.com/in/gabriel-razo-dantas-34724b301/',
     descricao:
-      'Contribui com o desenvolvimento da solução e com a estruturação técnica do avatar inteligente Lumën.',
+      'Contribui com o desenvolvimento do sistema de Java integrado em Crud e Dao.',
     imagem: '/image/razo.01.jpeg',
   },
 ]
